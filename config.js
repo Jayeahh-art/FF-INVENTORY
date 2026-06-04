a067300a8fd9f51293943e8f10d9928b8c12598f
Jayeahh-art <jayeahh.art@gmail.com>
2026-06-04 17:12:01 +0800

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste your deployed Apps Script Web App /exec URL here.
 window.CONFIG = {
-  API_URL: 'https://script.google.com/a/macros/finandfin.com/s/AKfycbyIkB5U-nGB2v_uRikMnML6aMY5SnyoVkc_LXrB4K3eAy5DLzl7nNejLrUTvLhv_Ggkww/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyIkB5U-nGB2v_uRikMnML6aMY5SnyoVkc_LXrB4K3eAy5DLzl7nNejLrUTvLhv_Ggkww/exec',
   APP_NAME: 'FF Inventory'
 };
